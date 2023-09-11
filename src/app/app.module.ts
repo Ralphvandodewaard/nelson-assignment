7c760ba3af33eae86f5afbe3c1262b76fc67607e
Ralphvandodewaard <rtvd@ziggo.nl>
2023-09-12 00:36:10 +0200

    Added product detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule
