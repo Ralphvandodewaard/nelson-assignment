8fc5d5e5a67af8a2a1b732a4d485f24078111c21
Ralphvandodewaard <rtvd@ziggo.nl>
2023-09-12 01:15:16 +0200

    Added api service and getting product

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api/api.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  product: any | null = null;
 
+  error: any | null = null;
+
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.getProduct();
+  }
+
+  getProduct(): void {
+    this.apiService.getProduct().subscribe({
+      next: (response) => {
+        this.product = response;
+      },
+      error: (error) => {
+        this.error = error;
+      }
+    })
+  }
 }
