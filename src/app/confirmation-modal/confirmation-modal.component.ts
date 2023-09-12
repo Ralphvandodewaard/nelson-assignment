5cdb3dbc20fe46144c8c5c76c710f3b9ba39acee
Ralphvandodewaard <rtvd@ziggo.nl>
2023-09-12 14:25:22 +0200

    Added product data to confirmation modal

--- src/app/confirmation-modal/confirmation-modal.component.ts
+++ src/app/confirmation-modal/confirmation-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-confirmation-modal',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./confirmation-modal.component.css']
 })
 export class ConfirmationModalComponent {
-
+  @Input() product!: Product;
 }
